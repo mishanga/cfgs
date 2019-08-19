4098e1b5e4f21b2d0c5c0f8832f94bbc0d141e6a
Mikhail Troshev <mishanga@yandex-team.ru>
2019-08-19 16:47:31 +0300

    удалил неактуальные файлы

--- copy.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-scp -r .ssh $1:~/
-scp -r cfgs $1:~/
-ssh $1 'cd && ln -sf cfgs/.vim .'
-ssh $1 'cd && ln -sf cfgs/.vimrc .'
-ssh $1 'cd && ln -sf cfgs/.gitconfig .'
-ssh $1 'cd && ln -sf cfgs/.profile .'
